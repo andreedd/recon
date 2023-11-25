0919434d59b795d80d19bf62817588e5466e19ae
André Nordström <ddeeh2@gmail.com>
2023-11-25 14:12:35 +0200

    update compose update script

--- update_docker_compose.py
+++ update_docker_compose.py
@@ -1,19 +1,31 @@
 import sys
-import yaml
+import re
 
 github_sha = sys.argv[1] if len(sys.argv) > 1 else None
+image_name = sys.argv[2] if len(sys.argv) > 2 else None
 
-if github_sha:
-    # Read the docker-compose.yml file
-    with open('docker-compose.yaml', 'r') as file:
-        compose_file = yaml.safe_load(file)
+if github_sha and image_name:
+    file_path = 'docker-compose.yml'
+    with open(file_path, 'r') as file:
+        content = file.read()
 
-    # Update the image tag for the nginx service
-    if 'services' in compose_file and 'nginx' in compose_file['services']:
-        compose_file['services']['nginx']['image'] = f"ddeeh/composegitops:{github_sha}"
+    # Define the pattern to find and update the image
+    pattern = re.compile(rf'image:\s*{re.escape(image_name)}:[^\n\r\s]+', re.MULTILINE)
 
-    # Write the updated docker-compose.yml file
-    with open('docker-compose.yml', 'w') as file:
-        yaml.dump(compose_file, file)
+    # Check if the pattern matches
+    match = re.search(pattern, content)
+    if match:
+        updated_content = re.sub(
+            pattern,
+            f"image: {image_name}:{github_sha}",
+            content
+        )
+
+        # Write the updated content back to the file
+        with open(file_path, 'w') as file:
+            file.write(updated_content)
+        print(f"Image tag for '{image_name}' updated to '{github_sha}'")
+    else:
+        print(f"No service found with image name '{image_name}'")
 else:
-    print("GitHub SHA not provided.")
+    print("GitHub SHA or image name not provided.")
